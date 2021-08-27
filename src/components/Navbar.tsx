1c482313623023fd2132f48e573141391a3adf83
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-28 02:51:19 +0530

    updated navbar height

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export const Navbar: React.FC<INavbarProps> = ({
 }) => {
   return (
     <nav className="bg-red-600 relative">
-      <div className="h-16 md:h-28 flex bg-white flex-wrap items-center justify-between p-4 border-b-2 border-gray-300">
+      <div className="h-1/5 md:h-28 flex bg-white flex-wrap items-center justify-between p-4 border-b-2 border-gray-300">
         <div className="flex gap-4 items-center justify-center">
           <div className={`block lg:${!showMobileNav && "hidden"}`}>
             <button
@@ -35,17 +35,17 @@ export const Navbar: React.FC<INavbarProps> = ({
           {children}
         </div>
         <div className="flex gap-2 items-center">
-          <Button className="py-2 px-2 bg-white hover:bg-gray-100 focus:ring-blue-500 focus:ring-offset-blue-200 text-blue-600 w-22 md:w-24 transition ease-in duration-200 text-center font-semibold focus:outline-none focus:ring-2 focus:ring-offset-0 rounded-full text-xs md:text-base">
+          <Button className="py-2 px-2 bg-white hover:bg-gray-100 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-indigo-600 w-22 md:w-24 transition ease-in duration-200 text-center font-semibold focus:outline-none focus:ring-2 focus:ring-offset-0 rounded-full text-xs md:text-base">
             Sign In
           </Button>
-          <Button className="py-2 px-2 bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-22 md:w-24 transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full text-xs md:text-base">
+          <Button className="py-2 px-2 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-22 md:w-24 transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full text-xs md:text-base">
             Sign Up
           </Button>
         </div>
       </div>
       {/* MOBILE NAV */}
       <div
-        className={`absolute w-full h-12 bg-purple-300 ${
+        className={`absolute w-full h-12 bg-purple-100 ${
           showMobileNav ? "flex" : "hidden"
         } lg:hidden gap-6 items-center justify-center text-xl`}
       >
