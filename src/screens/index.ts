df6854f7c617becdc9067648c14c18cc2e9d8333
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-29 03:07:51 +0530

    updated Blog imports

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,5 +3,13 @@ import { CompanyLogos } from "./CompanyLogos";
 import { FeaturesCardsAtGlance } from "./FeaturesCardsAtGlance";
 import { Hero } from "./Hero";
 import { Team } from "./Team";
+import { Blogs } from "./Blogs";
 
-export { FeaturesCardsAtGlance, DetailedFeatures, CompanyLogos, Team, Hero };
+export {
+  FeaturesCardsAtGlance,
+  DetailedFeatures,
+  CompanyLogos,
+  Team,
+  Hero,
+  Blogs,
+};
