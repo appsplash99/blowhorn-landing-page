1936b00995dd54c35ea1f8ecfb1680db8238adba
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-29 01:31:00 +0530

    updated company logos

---
+++ src/screens/CompanyLogos.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { COMPANIES_INFO } from "../utils/companiesInfo";
+
+export const CompanyLogos = () => {
+  return (
+    <section className="bg-white pt-7 pb-14">
+      <div className="container px-8 mx-auto sm:px-12 lg:px-20">
+        <h1 className="text-sm font-bold tracking-wide text-center text-gray-800 uppercase mb-7">
+          Trusted by top-leading companies.
+        </h1>
+        <div className="grid items-center justify-center grid-cols-12 gap-y-8">
+          {COMPANIES_INFO.map((company, index) => (
+            <div className="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+              <img
+                alt={company.name}
+                src={company.imageSrc}
+                className="block object-contain h-12"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/utils/companiesInfo.ts
@@ -0,0 +1,31 @@
+interface ICOMPANY {
+  name: string;
+  imageSrc: string;
+}
+
+export const COMPANIES_INFO: ICOMPANY[] = [
+  {
+    name: "Disney Plus",
+    imageSrc: "https://cdn.devdojo.com/tails/images/disney-plus.svg",
+  },
+  {
+    name: "Google",
+    imageSrc: "https://cdn.devdojo.com/tails/images/google.svg",
+  },
+  {
+    name: "Hubspot",
+    imageSrc: "https://cdn.devdojo.com/tails/images/hubspot.svg",
+  },
+  {
+    name: "Youtube",
+    imageSrc: "https://cdn.devdojo.com/tails/images/youtube.svg",
+  },
+  {
+    name: "Slack",
+    imageSrc: "https://cdn.devdojo.com/tails/images/slack.svg",
+  },
+  {
+    name: "Shopify",
+    imageSrc: "https://cdn.devdojo.com/tails/images/shopify.svg",
+  },
+];
