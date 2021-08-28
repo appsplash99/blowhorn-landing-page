bee75ba8e4aa67871310a2f1fb4546d597e16180
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-29 01:53:43 +0530

    added Team Details and component

---
+++ src/screens/Team.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { AiOutlineTwitter, AiOutlineGithub } from "react-icons/ai";
+import { TEAM } from "../utils/teamDetails";
+
+export const Team = () => {
+  return (
+    <>
+      <section className="relative py-20 overflow-hidden bg-white">
+        <span className="absolute top-0 right-0 flex flex-col items-end mt-0 -mr-16 opacity-60">
+          <span className="container hidden w-screen h-32 max-w-xs mt-20 transform rounded-full rounded-r-none md:block md:max-w-xs lg:max-w-lg 2xl:max-w-3xl bg-blue-50" />
+        </span>
+        <span className="absolute bottom-0 left-0"> </span>
+        <div className="relative px-16 mx-auto max-w-7xl">
+          <p className="font-medium tracking-wide text-blue-500 uppercase">
+            OUR TEAM
+          </p>
+          <h2 className="relative max-w-lg mt-5 mb-10 text-4xl font-semibold leading-tight lg:text-5xl">
+            An incredible team of <br />
+            amazing individuals
+          </h2>
+          <div className="grid w-full grid-cols-2 gap-10 sm:grid-cols-3 lg:grid-cols-4">
+            {TEAM.map((teamMate, index) => (
+              <div className="flex flex-col items-center justify-center col-span-1">
+                <div className="relative p-5">
+                  <div className="absolute z-10 w-full h-full -mt-5 -ml-5 rounded-full rounded-tr-none bg-blue-50" />
+                  <img
+                    className="relative z-20 w-full rounded-full"
+                    src={teamMate.imageSrc}
+                  />
+                </div>
+                <div className="mt-3 space-y-2 text-center">
+                  <div className="space-y-1 text-lg font-medium leading-6">
+                    <h3>{teamMate.name}</h3>
+                    <p className="text-blue-600">{teamMate.profession}</p>
+                  </div>
+                  <div className="relative flex items-center justify-center space-x-3">
+                    <AiOutlineTwitter className="text-gray-300 hover:text-gray-400" />
+                    <AiOutlineGithub className="text-gray-300 hover:text-gray-400" />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
---
+++ src/utils/teamDetails.ts
@@ -0,0 +1,48 @@
+interface ITeamMate {
+  name: string;
+  imageSrc: string;
+  profession: string;
+}
+
+export const TEAM: ITeamMate[] = [
+  {
+    name: "Freddy Smith",
+    profession: "CEO and Founder",
+    imageSrc: "https://cdn.devdojo.com/images/june2021/avt-03.jpg",
+  },
+  {
+    name: "Carl Jones",
+    profession: "CTO and Co-Founder",
+    imageSrc: "https://cdn.devdojo.com/images/june2021/avt-07.jpg",
+  },
+  {
+    name: "Susan Peterson",
+    profession: "Marketing Directory",
+    imageSrc: "https://cdn.devdojo.com/images/june2021/avt-20.jpg",
+  },
+  {
+    name: "Tommy Barnes",
+    profession: "Designer",
+    imageSrc: "https://cdn.devdojo.com/images/june2021/avt-09.jpg",
+  },
+  {
+    name: "Ron Jenson",
+    profession: "Senior Developer",
+    imageSrc: "https://cdn.devdojo.com/images/june2021/avt-14.jpg",
+  },
+  {
+    name: "Pete Tompkins",
+    profession: "Web Developer",
+    imageSrc: "https://cdn.devdojo.com/images/june2021/avt-13.jpg",
+  },
+  {
+    name: "Kelly Richards",
+    profession: "Sales Manager",
+    imageSrc: "https://cdn.devdojo.com/images/june2021/avt-16.jpg",
+  },
+  {
+    name: "Alexis Jordan",
+    profession: "Affiliate Manager",
+    imageSrc: "https://cdn.devdojo.com/images/june2021/avt-08.jpg",
+  },
+];
