e1d9041201db8eae31eb64dc7fc660782953d26f
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-29 03:09:19 +0530

    updated DetailedFeaturesCard component

--- src/components/DetailedFeaturesCard.tsx
+++ src/components/DetailedFeaturesCard.tsx
@@ -10,9 +10,13 @@ export const DetailedFeaturesCard: React.FC<IDetailedFeaturesCard> = ({
   description,
   title,
   icon,
+  ...props
 }) => {
   return (
-    <div className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl">
+    <div
+      {...props}
+      className="flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 bg-gray-100 sm:rounded-xl"
+    >
       <div className="p-3 text-white bg-blue-500 rounded-full">{icon}</div>
       <h4 className="text-xl font-medium text-gray-700">{title}</h4>
       <p className="text-base text-center text-gray-500">{description}</p>
