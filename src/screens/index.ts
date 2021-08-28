49c6aa2a132cbf366b6a176e348be481475c965b
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-29 03:22:33 +0530

    added Footer import

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,6 +2,7 @@ import { DetailedFeatures } from "./DetailedFeatures";
 import { CompanyLogos } from "./CompanyLogos";
 import { FeaturesCardsAtGlance } from "./FeaturesCardsAtGlance";
 import { Hero } from "./Hero";
+import { Footer } from "./Footer";
 import { Team } from "./Team";
 import { Blogs } from "./Blogs";
 
@@ -9,6 +10,7 @@ export {
   FeaturesCardsAtGlance,
   DetailedFeatures,
   CompanyLogos,
+  Footer,
   Team,
   Hero,
   Blogs,
