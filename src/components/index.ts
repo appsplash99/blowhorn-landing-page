679a96e4714695067f69d04a1259e9c43c5e417c
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-29 02:01:01 +0530

    updated imports

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import { Button } from "./Button";
+import { CustomFeatureIcon } from "./CustomFeatureIcon";
+import { DetailedFeatureRow } from "./DetailedFeatureRow";
+import { DetailedFeaturesCard } from "./DetailedFeaturesCard";
+import { Navbar } from "./Navbar";
+import { Section } from "./Section";
+
+export {
+  DetailedFeaturesCard,
+  DetailedFeatureRow,
+  CustomFeatureIcon,
+  Section,
+  Button,
+  Navbar,
+};
---
+++ src/screens/index.ts
@@ -0,0 +1,7 @@
+import { DetailedFeatures } from "./DetailedFeatures";
+import { CompanyLogos } from "./CompanyLogos";
+import { FeaturesCardsAtGlance } from "./FeaturesCardsAtGlance";
+import { Hero } from "./Hero";
+import { Team } from "./Team";
+
+export { FeaturesCardsAtGlance, DetailedFeatures, CompanyLogos, Team, Hero };
