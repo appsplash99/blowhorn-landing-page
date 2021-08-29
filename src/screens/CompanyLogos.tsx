887da93cfdbb399ecd14819aff5e23974baa777c
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-29 08:59:24 +0530

    updated CompanyLogos.tsx

--- src/screens/CompanyLogos.tsx
+++ src/screens/CompanyLogos.tsx
@@ -8,18 +8,14 @@ export const CompanyLogos = () => {
         <h1 className="text-sm font-bold tracking-wide text-center text-gray-800 uppercase mb-7">
           Trusted by top-leading companies.
         </h1>
-        <div className="grid items-center justify-center grid-cols-12 gap-y-8">
+        <div className="flex items-center justify-center flex-wrap gap-10">
           {COMPANIES_INFO.map((company, index) => (
-            <div
+            <img
               key={index}
-              className="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2"
-            >
-              <img
-                alt={company.name}
-                src={company.imageSrc}
-                className="block object-contain h-12"
-              />
-            </div>
+              alt={company.name}
+              src={company.imageSrc}
+              className="block object-contain w-24 md:w-32"
+            />
           ))}
         </div>
       </div>
