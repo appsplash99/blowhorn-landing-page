e6f7aa4d125a4e132e05a8cb5afbb9f6d924bbbb
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-29 13:06:58 +0530

    added checkCurrentRoute util

--- src/utils/utilFunctions.ts
+++ src/utils/utilFunctions.ts
@@ -3,3 +3,9 @@ export const getRandomBgColor = (array: string[]) => {
     Math.random() * (max - min) + min;
   return array[Math.floor(Math.floor(random(1, array.length)) - 1)];
 };
+
+export const checkExtensionOfCurrentUrl = (url: string) => {
+  const arr = url.split("/");
+  const lastItem = arr[arr.length - 1];
+  return lastItem;
+};
