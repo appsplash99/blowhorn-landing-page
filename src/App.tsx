71dd40442f3ec2cf08f03edee1f4abb6937bd846
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-29 13:10:48 +0530

    finalised App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,25 @@
-import React, { useState } from "react";
-import { AiOutlineSearch } from "react-icons/ai";
-import { Navbar } from "./components/Navbar";
-import "./App.css";
-import { Hero } from "./components/screens/Hero";
+import React from "react";
+import {
+  FeaturesCardsAtGlance,
+  DetailedFeatures,
+  CompanyLogos,
+  Footer,
+  Blogs,
+  Team,
+  Hero,
+} from "./screens";
 
-const App: React.FC = () => {
-  const [showMobileNav, setshowMobileNav] = useState<boolean>(false);
+const App = () => {
   return (
-    <div className="bg-white md:px-16">
-      <Navbar showMobileNav={showMobileNav} setshowMobileNav={setshowMobileNav}>
-        <>
-          <a
-            href="/"
-            className="text-gray-600 focus:text-blue-600 font-medium"
-            onClick={(e) => e.preventDefault()}
-          >
-            Home
-          </a>
-          <a
-            href="/features"
-            className="text-gray-600 focus:text-blue-600 font-medium"
-            onClick={(e) => e.preventDefault()}
-          >
-            Features
-          </a>
-          <a
-            href="/blog"
-            className="text-gray-600 focus:text-blue-600 font-medium"
-            onClick={(e) => e.preventDefault()}
-          >
-            Blog
-          </a>
-          <a
-            href="/contact"
-            className="text-gray-600 focus:text-blue-600 font-medium"
-            onClick={(e) => e.preventDefault()}
-          >
-            Contact
-          </a>
-          <a
-            href="/search"
-            className="text-gray-600 focus:text-blue-600 font-medium"
-            onClick={(e) => e.preventDefault()}
-          >
-            <AiOutlineSearch
-              className="text-gray-600 focus:text-blue-600 font-medium"
-              onClick={(e) => e.preventDefault()}
-            />
-          </a>
-        </>
-      </Navbar>
+    <>
       <Hero />
-    </div>
+      <CompanyLogos />
+      <DetailedFeatures />
+      <FeaturesCardsAtGlance />
+      <Team />
+      <Blogs />
+      <Footer />
+    </>
   );
 };
 
