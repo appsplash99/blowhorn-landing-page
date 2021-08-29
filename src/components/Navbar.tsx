b3de9eb0c0286a577a8cd8b2063a31f5825779bf
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-08-29 08:57:51 +0530

    updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
-import { Button } from "./Button";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaBars, FaWindowClose } from "react-icons/fa";
+import { MENU_LINKS } from "../utils/menuLinks";
 
 interface INavbarProps {
   showMobileNav: boolean;
@@ -8,48 +9,81 @@ interface INavbarProps {
 }
 
 export const Navbar: React.FC<INavbarProps> = ({
-  children,
-  showMobileNav,
   setshowMobileNav,
+  showMobileNav,
 }) => {
   return (
-    <nav className="absolute w-screen md:px-16">
-      <div className="h-1/5 md:h-28 flex bg-white flex-wrap items-center justify-between p-4 border-b-2 border-gray-300">
-        <div className="flex gap-4 items-center justify-center">
-          <div className={`block lg:${!showMobileNav && "hidden"}`}>
-            <button
-              className="navbar-burger flex items-center py-2 px-3 text-indigo-500 rounded border border-indigo-500"
-              onClick={() => setshowMobileNav(!showMobileNav)}
-            >
-              {showMobileNav ? <IoIosArrowDown /> : <IoIosArrowForward />}
-            </button>
-          </div>
-          <a className="text-2xl text-gray-800 font-bold font-heading" href="#">
-            nullBrains.
+    <nav className={`relative z-50 h-24 select-none`}>
+      <div className="container relative flex flex-wrap items-center justify-between h-24 mx-auto overflow-hidden font-medium border-b border-gray-200 md:overflow-visible lg:justify-center sm:px-4 md:px-2">
+        <div className="flex items-center justify-start w-1/4 h-full pr-4">
+          <a href="#_" className="inline-block py-4 md:py-0">
+            <span className="p-1 text-xl font-black leading-none text-gray-900">
+              <span>nullBrains.</span>
+              <span className="text-indigo-600">.</span>
+            </span>
           </a>
         </div>
-        {/* DESKTOP MENU */}
         <div
-          className={`hidden lg:flex gap-6 items-center justify-center text-xl`}
+          className={`top-0 left-0 right-0 items-start w-full h-full p-4 text-sm bg-gray-900 bg-opacity-50 md:items-center md:w-3/4 md:absolute lg:text-base md:bg-transparent md:p-0 md:relative md:flex transition-all duration-200 ease-in-out transform  ${
+            showMobileNav ? "flex fixed" : "hidden"
+          }`}
         >
-          {children}
+          <div
+            className={`flex-col w-full h-auto overflow-hidden bg-white rounded-lg md:bg-transparent md:overflow-visible md:rounded-none md:relative md:flex md:flex-row `}
+          >
+            <a
+              href="#_"
+              className="inline-flex items-center block w-auto h-16 px-6 text-xl font-black leading-none text-gray-900 md:hidden"
+            >
+              nullBrains.<span className="text-indigo-600">.</span>
+            </a>
+            <div className="flex flex-col items-start justify-center w-full space-x-6 text-center lg:space-x-8 md:w-2/3 md:mt-0 md:flex-row md:items-center">
+              {Object.keys(MENU_LINKS).map((key, index) => (
+                <a
+                  key={index}
+                  href="#_"
+                  onClick={(e) => e.preventDefault()}
+                  className="inline-block w-full py-2 mx-0 ml-6 font-medium text-left text-indigo-600 md:ml-0 md:w-auto md:px-0 md:mx-2 lg:mx-3 md:text-center"
+                >
+                  {key}
+                </a>
+              ))}
+              <a
+                href="#_"
+                className="absolute top-0 left-0 hidden py-2 mt-6 ml-10 mr-2 text-gray-600 lg:inline-block md:mt-0 md:ml-2 lg:mx-3 md:relative"
+              >
+                <AiOutlineSearch
+                  className="text-gray-600 hover:underline focus:text-indigo-600 font-medium"
+                  onClick={(e) => e.preventDefault()}
+                />
+              </a>
+            </div>
+            <div className="flex flex-col items-start justify-end w-full pt-4 md:items-center md:w-1/3 md:flex-row md:py-0">
+              <a
+                href="#"
+                className="w-full px-6 py-2 mr-0 text-gray-700 md:px-0 lg:pl-2 md:mr-4 lg:mr-5 md:w-auto"
+              >
+                Sign In
+              </a>
+              <a
+                href="#_"
+                className="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-indigo-600 md:px-3 md:w-auto md:rounded-full lg:px-5 hover:bg-indigo-500 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-indigo-600"
+              >
+                Sign Up
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="flex gap-2 items-center">
-          <Button className="py-2 px-2 bg-white hover:bg-gray-100 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-indigo-600 w-22 md:w-24 transition ease-in duration-200 text-center font-semibold focus:outline-none focus:ring-2 focus:ring-offset-0 rounded-full text-xs md:text-base">
-            Sign In
-          </Button>
-          <Button className="py-2 px-2 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-22 md:w-24 transition ease-in duration-200 text-center font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-full text-xs md:text-base">
-            Sign Up
-          </Button>
+        <div
+          className="absolute right-0 flex flex-col items-center items-end justify-center w-10 h-10 bg-white rounded-full cursor-pointer md:hidden hover:bg-gray-100"
+          onClick={() => setshowMobileNav(!showMobileNav)}
+        >
+          {!showMobileNav ? (
+            <FaBars className="text-xl" />
+          ) : (
+            <FaWindowClose className="text-xl" />
+          )}
         </div>
-      </div>
-      {/* MOBILE NAV */}
-      <div
-        className={`w-full h-12 bg-purple-100 ${
-          showMobileNav ? "flex" : "hidden"
-        } lg:hidden gap-6 items-center justify-center text-xl`}
-      >
-        {children}
       </div>
     </nav>
   );
